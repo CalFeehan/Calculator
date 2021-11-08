74e78667e38d14557c7e5faff06a7f0c6a21f328
Callum Feehan <72271107+CalFeehan@users.noreply.github.com>
2021-11-08 15:43:28 +0000

    Calculator multiplication

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,9 +4,45 @@ namespace Calculator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number1;
+            while (true)
+            {
+                Console.Write("Please enter first number: ");
+                string userNumber1 = Console.ReadLine();
+                try
+                {
+                    number1 = Int32.Parse(userNumber1);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Whole numbers only please.");
+                    continue;
+                }
+                break;
+            }
+
+            int number2;
+            while (true)
+            {
+                Console.Write("Please enter second number: ");
+                string userNumber2 = Console.ReadLine();
+                try
+                {
+                    number2 = Int32.Parse(userNumber2);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Whole numbers only please.");
+                    continue;
+                }
+                break;
+            }
+
+            Console.Write(number1 * number2);
+
         }
     }
 }
